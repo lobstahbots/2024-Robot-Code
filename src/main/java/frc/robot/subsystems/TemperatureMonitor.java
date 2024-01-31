6c4cc7fe23d088eef5c8267ea4cbf5f1bdda3ff4
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-01-31 15:14:28 -0500

    Add temperature monitor

---
+++ src/main/java/frc/robot/subsystems/TemperatureMonitor.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.List;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants.TempConstants;
+
+/**
+ * Class to monitor the temperature of some items.
+ */
+public class TemperatureMonitor {
+    public static interface Monitorable {
+        public void setDisabled(boolean disable);
+        public boolean getDisabled();
+        public double getMotorTemperature();
+    }
+    private double overheatTime = -1;
+    private boolean disabled = false;
+    private List<Monitorable> motors;
+
+    TemperatureMonitor(List<Monitorable> motors) {
+        this.motors = motors;
+    }
+
+    public void monitor() {
+        boolean safe = true;
+        for (Monitorable motor : motors) {
+            double temp = motor.getMotorTemperature();
+            if (temp > TempConstants.SAFE_TEMP) safe = false;
+            if (temp > TempConstants.OVERHEAT_TEMP && overheatTime == -1) overheatTime = Timer.getFPGATimestamp();
+            else if (temp > TempConstants.OVERHEAT_TEMP && Timer.getFPGATimestamp() - overheatTime >= 2) {
+                disabled = true;
+            }
+            if (disabled) {
+                motor.setDisabled(true);
+            }
+        }
+        if (safe) {
+            disabled = false;
+            overheatTime = -1;
+        }
+    }
+}
