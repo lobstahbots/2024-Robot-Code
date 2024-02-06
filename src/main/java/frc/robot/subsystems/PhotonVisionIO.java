60f18f7daaefeff0bf74b5885cb48cc7e246d6b9
ChiliRhino417 <teddyshi19@gmail.com>
2024-02-06 12:18:34 -0500

    Fixes the values in PhotonVisionIOInputs

--- src/main/java/frc/robot/subsystems/PhotonVisionIO.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionIO.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.littletonrobotics.junction.AutoLog;
@@ -13,43 +14,11 @@ public interface PhotonVisionIO {
             null, 0, null, null);
         public EstimatedRobotPose estimatedRearPose = new EstimatedRobotPose(
             null, 0, null, null);
+        public List<PhotonTrackedTarget> frontTargets = new ArrayList<>();
+        public List<PhotonTrackedTarget> rearTargets = new ArrayList<>();
+        public List<Integer> visibleFrontFiducialIDs = new ArrayList<>();
+        public List<Integer> visibileRearFiducialIDs = new ArrayList<>();  
     }
-
-    /**
-     * Get the estimated pose from the front camera.
-     * @return The estimated pose.
-     */
-    public EstimatedRobotPose getEstimatedFrontPose();
-    
-    /**
-     * Get the estimated pose from the rear camera.
-     * @return The estimated pose.
-     */
-    public EstimatedRobotPose getEstimatedRearPose();
-
-    /**
-     * Get the tracked targets from the front camera.
-     * @return The tracked targets.
-     */
-    public List<PhotonTrackedTarget> getFrontTargets();
-
-    /**
-     * Get the tracked targets from the rear camera.
-     * @return The tracked targets.
-     */
-    public List<PhotonTrackedTarget> getRearTargets();
-
-    /**
-     * Get the fiducial IDs of the targets in the front camera.
-     * @return A list of the IDs.
-     */
-    public List<Integer> getFrontFiducialIDs();
-
-    /**
-     * Get the fiducial IDs of the targets in the rear camera.
-     * @return A list of the IDs.
-     */
-    public List<Integer> getRearFiducialIDs();
 
     public default void updateInputs(PhotonVisionIOInputs inputs) {}
 }
