64f1a4050f9aef2718fbf19c169db83f452e79b7
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-02-15 02:35:01 -0500

    rename "trajectoryFactory" to "autoFactory"

---
+++ src/main/java/frc/robot/AutoFactory.java
@@ -0,0 +1,187 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Volts;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants.PathConstants;
+import frc.robot.subsystems.drive.DriveBase;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.pivot.Pivot;
+import frc.robot.subsystems.shooter.Shooter;
+import stl.sysId.CharacterizableSubsystem;
+
+public class AutoFactory {
+    private final Supplier<List<Object>> responses;
+    private final DriveBase driveBase;
+    private final Intake intake;
+    private final Shooter shooter;
+
+    public AutoFactory(DriveBase driveBase, Shooter shooter, Intake intake, Pivot pivot,
+            Supplier<List<Object>> responsesSupplier) {
+        this.responses = responsesSupplier;
+        this.driveBase = driveBase;
+        this.intake = intake;
+        this.shooter = shooter;
+    }
+
+    /**
+     * Determines type of an inputted trajectory - constructed using Choreo, or
+     * using PathPlanner. Default should be Choreo.
+     */
+    public enum PathType {
+        CHOREO,
+        PATHPLANNER
+    }
+
+    /**
+     * Constructs a path following command to generate a path to a target position.
+     * 
+     * @param targetPose The desired end pose of the generated path.
+     * @return The constructed path following command
+     */
+    public Command getPathFindToPoseCommand(Pose2d targetPose) {
+
+        // Since AutoBuilder is configured, we can use it to build pathfinding commands
+        Command pathfindingCommand = AutoBuilder.pathfindToPose(
+                targetPose,
+                PathConstants.CONSTRAINTS,
+                0.0, // Goal end velocity in meters/sec
+                0.0 // Rotation delay distance in meters. This is how far the robot should travel
+                    // before attempting to rotate.
+        );
+
+        return pathfindingCommand;
+    }
+
+    /**
+     * Constructs a path following command to a preset path from the deploy
+     * directory Path can be PathPlanner or Choreo-constructed.
+     * 
+     * @param pathname A String containing the name of the file with the path (leave
+     *                 out the .traj or .path ending).
+     * @param pathType A {@link PathType} determining the format of the inputted
+     *                 trajectory. Files ending in .path should be imported as
+     *                 PATHPLANNER, while files ending in .traj should be imported
+     *                 as CHOREO.
+     * @return The constructed path following command
+     */
+    public Command getPathFindToPathCommand(String pathname, PathType pathType) {
+        PathPlannerPath path;
+        switch (pathType) {
+            case CHOREO:
+                path = PathPlannerPath.fromChoreoTrajectory(pathname);
+                break;
+            case PATHPLANNER:
+                path = PathPlannerPath.fromPathFile(pathname);
+                break;
+            default:
+                path = PathPlannerPath.fromChoreoTrajectory(pathname);
+        }
+
+        // Since AutoBuilder is configured, we can use it to build pathfinding commands
+        Command pathfindingCommand = AutoBuilder.pathfindThenFollowPath(
+                path,
+                PathConstants.CONSTRAINTS,
+                3.0 // Rotation delay distance in meters. This is how far the robot should travel
+                    // before attempting to rotate.
+        );
+
+        return pathfindingCommand;
+    }
+
+    /**
+     * Constructs a path following command through a provided set of waypoints. Ends
+     * with desired holonomic rotation.
+     * 
+     * @param goalEndRotationHolonomic Desired holonomic end rotation
+     * @param poses                    List of bezier poses. Each {@link Pose2d}
+     *                                 represents one waypoint. The rotation
+     *                                 component of the pose should be the direction
+     *                                 of travel. Do not use holonomic rotation.
+     * @return The constructed path following command through provided poses, with
+     *         set end rotation.
+     */
+    public Command getPathFromWaypoints(Rotation2d goalEndRotationHolonomic, Pose2d... poses) {
+        List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(poses);
+
+        // Create the path using the bezier points created above
+        PathPlannerPath path = new PathPlannerPath(
+                bezierPoints, PathConstants.CONSTRAINTS,
+                new GoalEndState(0.0, goalEndRotationHolonomic) // Goal end state. You can set a holonomic rotation
+                                                                // here. If using a differential drivetrain, the
+                                                                // rotation will have no effect.
+        );
+
+        Command pathCommand = AutoBuilder.pathfindThenFollowPath(
+                path,
+                PathConstants.CONSTRAINTS,
+                3.0 // Rotation delay distance in meters. This is how far the robot should travel
+                    // before attempting to rotate.
+        );
+        return pathCommand;
+    }
+
+    public Command getSimpleAuto() {
+        String pathname = "";
+        Pose2d startingPosition = (Pose2d) responses.get().get(0);
+        if (startingPosition == PathConstants.STATION_1) {
+            pathname = "Station1SimpleAuto";
+        } else if (startingPosition == PathConstants.STATION_2) {
+            pathname = "Station2SimpleAuto";
+        } else if (startingPosition == PathConstants.STATION_3) {
+            pathname = "Station3SimpleAuto";
+        }
+        System.out.println("Pathfinding " + pathname);
+        return getPathFindToPathCommand(pathname, PathType.CHOREO);
+    }
+
+    public enum CharacterizationRoutine {
+        QUASISTATIC_FORWARD,
+        QUASISTATIC_BACKWARD,
+        DYNAMIC_FORWARD,
+        DYNAMIC_BACKWARD,
+    }
+
+    public Command getCharacterizationRoutine() {
+        System.out.println(responses.get().size());
+        CharacterizableSubsystem subsystem = (CharacterizableSubsystem) responses.get().get(1);
+        CharacterizationRoutine routine = (CharacterizationRoutine) responses.get().get(0);
+
+        var sysIdRoutine = new SysIdRoutine(
+                new SysIdRoutine.Config(
+                        null, null, null, // Use default config
+                        (state) -> Logger.recordOutput("SysIdTestState", state.toString())),
+                new SysIdRoutine.Mechanism(
+                        (Measure<Voltage> voltage) -> subsystem.runVolts(voltage.in(Volts)),
+                        null, // No log consumer, since data is recorded by AdvantageKit
+                        subsystem));
+        switch (routine) {
+            case QUASISTATIC_FORWARD:
+                return sysIdRoutine.quasistatic(SysIdRoutine.Direction.kForward);
+            case QUASISTATIC_BACKWARD:
+                return sysIdRoutine.quasistatic(SysIdRoutine.Direction.kReverse);
+            case DYNAMIC_FORWARD:
+                return sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward);
+            case DYNAMIC_BACKWARD:
+                return sysIdRoutine.dynamic(SysIdRoutine.Direction.kReverse);
+            default:
+                return new WaitCommand(1);
+        }
+    }
+}
