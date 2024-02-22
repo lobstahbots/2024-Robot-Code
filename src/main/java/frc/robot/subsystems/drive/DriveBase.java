a700802faafded7f91d4ef8c56c5b859d817b0c6
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-02-22 03:07:57 -0500

    add camera transforms

--- src/main/java/frc/robot/subsystems/drive/DriveBase.java
+++ src/main/java/frc/robot/subsystems/drive/DriveBase.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.drive;
+
 import java.util.Optional;
 
 import org.littletonrobotics.junction.Logger;
@@ -17,11 +18,15 @@ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.geometry.Twist3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -35,18 +40,17 @@ import stl.sysId.CharacterizableSubsystem;
 
 public class DriveBase extends CharacterizableSubsystem {
   /** Creates a new SwerveDriveBase. */
-  private final SwerveModule[] modules; 
+  private final SwerveModule[] modules;
 
   private SwerveDrivePoseEstimator swerveOdometry;
   private SwerveSetpointGenerator setpointGenerator;
-  private SwerveSetpoint swerveSetpoint =
-  new SwerveSetpoint(
+  private SwerveSetpoint swerveSetpoint = new SwerveSetpoint(
       new ChassisSpeeds(),
       new SwerveModuleState[] {
-        new SwerveModuleState(),
-        new SwerveModuleState(),
-        new SwerveModuleState(),
-        new SwerveModuleState()
+          new SwerveModuleState(),
+          new SwerveModuleState(),
+          new SwerveModuleState(),
+          new SwerveModuleState()
       });
   private final GyroIO gyro;
   private final GyroIOInputsAutoLogged gyroInputs = new GyroIOInputsAutoLogged();
@@ -56,50 +60,37 @@ public class DriveBase extends CharacterizableSubsystem {
 
   private Field2d field;
 
-  public DriveBase(GyroIO gyroIO, PhotonVision photonVision, SwerveModuleIO frontLeft, SwerveModuleIO frontRight, SwerveModuleIO backLeft, SwerveModuleIO backRight, boolean isOpenLoop) {
+  public DriveBase(GyroIO gyroIO, PhotonVision photonVision, SwerveModuleIO frontLeft, SwerveModuleIO frontRight,
+      SwerveModuleIO backLeft, SwerveModuleIO backRight, boolean isOpenLoop) {
 
-    this.modules = new SwerveModule[]{new SwerveModule(frontLeft, 0), new SwerveModule(frontRight, 1), new SwerveModule(backLeft, 2), new SwerveModule(backRight, 3)};
-    
+    this.modules = new SwerveModule[] { new SwerveModule(frontLeft, 0), new SwerveModule(frontRight, 1),
+        new SwerveModule(backLeft, 2), new SwerveModule(backRight, 3) };
+
     this.gyro = gyroIO;
 
     gyro.zeroGyro();
     this.photonVision = photonVision;
-    swerveOdometry = new SwerveDrivePoseEstimator(DriveConstants.KINEMATICS, gyroInputs.yawPosition, getPositions(), new Pose2d());
+    swerveOdometry = new SwerveDrivePoseEstimator(DriveConstants.KINEMATICS, gyroInputs.yawPosition, getPositions(),
+        new Pose2d());
     setpointGenerator = new SwerveSetpointGenerator(DriveConstants.KINEMATICS, DriveConstants.MODULE_LOCATIONS);
-    
+
     field = new Field2d();
     SmartDashboard.putData("Field", field);
 
     this.isOpenLoop = isOpenLoop;
-
-    AutoBuilder.configureHolonomic(
-        this::getPose, // Robot pose supplier
-        this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-            new PIDConstants(1, 0.0, 0.0), // Translation PID constants
-            new PIDConstants(1, 0.0, 0.0), // Rotation PID constants
-            4.5, // Max module speed, in m/s
-            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-            new ReplanningConfig() // Default path replanning config. See the API for the options here
-        ),
-        () -> {return DriverStation.getAlliance().get() ==  DriverStation.Alliance.Red;},
-      this 
-    );
-
-    NoteVisualizer.setRobotPoseSupplier(this::getPose);
   }
 
-  /**Resets pose of odometry to a given pose.
+  /**
+   * Resets pose of odometry to a given pose.
    * 
    * @param pose The desired pose to reset the odometry to.
    */
   public void resetPose(Pose2d pose) {
     swerveOdometry.resetPosition(gyroInputs.yawPosition, getPositions(), pose);
   }
-  
-  /**Gets pose from odometry.
+
+  /**
+   * Gets pose from odometry.
    * 
    * @return The current estimated pose of the odometry
    */
@@ -107,9 +98,11 @@ public class DriveBase extends CharacterizableSubsystem {
     return swerveOdometry.getEstimatedPosition();
   }
 
-  /**Gets states of the four swerve modules.
+  /**
+   * Gets states of the four swerve modules.
    * 
-   * @return The states of the four swerve modules in a {@link SwerveModuleState} array.
+   * @return The states of the four swerve modules in a {@link SwerveModuleState}
+   *         array.
    */
   public SwerveModuleState[] getStates() {
     SwerveModuleState[] states = new SwerveModuleState[4];
@@ -119,9 +112,11 @@ public class DriveBase extends CharacterizableSubsystem {
     return states;
   }
 
-  /**Gets positions of the four swerve modules.
+  /**
+   * Gets positions of the four swerve modules.
    * 
-   * @return The positions of the four swerve modules in a {@link SwerveModulePosition} array.
+   * @return The positions of the four swerve modules in a
+   *         {@link SwerveModulePosition} array.
    */
   public SwerveModulePosition[] getPositions() {
     SwerveModulePosition[] positions = new SwerveModulePosition[4];
@@ -130,116 +125,125 @@ public class DriveBase extends CharacterizableSubsystem {
     }
     return positions;
   }
- 
-  /**Gets robot relative ChassisSpeeds.
+
+  /**
+   * Gets robot relative ChassisSpeeds.
    * 
    * @return The robot-relative {@link ChassisSpeeds}.
    */
   public ChassisSpeeds getRobotRelativeSpeeds() {
-  return ChassisSpeeds.fromFieldRelativeSpeeds(DriveConstants.KINEMATICS.toChassisSpeeds(getStates()), gyroInputs.yawPosition);
- } 
-
-/**
- * Drives the robot robot-relative according to provided {@link ChassisSpeeds}.
- * 
- * @param chassisSpeeds The desired ChassisSpeeds. Should be robot relative.
- */
-public void driveRobotRelative(ChassisSpeeds chassisSpeeds) {
-  ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(chassisSpeeds, 0.02);
-  Logger.recordOutput("Unoptimized:", DriveConstants.KINEMATICS.toSwerveModuleStates(discreteSpeeds));
-  swerveSetpoint = setpointGenerator.generateSetpoint(DriveConstants.MODULE_LIMITS, swerveSetpoint, discreteSpeeds, SimConstants.LOOP_TIME);
-  SwerveDriveKinematics.desaturateWheelSpeeds(swerveSetpoint.moduleStates, DriveConstants.MAX_DRIVE_SPEED);
-  setModuleStates(swerveSetpoint.moduleStates);
-}
-
-/**Sets desired SwerveModuleStates. Optimizes states.
- * 
- * @param desiredStates The states to set for each module.
- * @return The optimized SwerveModuleStates, now desired states.
- */
-public SwerveModuleState[] setModuleStates(SwerveModuleState[] desiredStates) {
-  SwerveModuleState[] optimizedStates = new SwerveModuleState[4];
-  // SwerveDriveKinematics.desaturateWheelSpeeds(
-  //     desiredStates, DriveConstants.MAX_DRIVE_SPEED);
-  for(SwerveModule module: modules) {
-    optimizedStates[module.getModuleID()] = module.setDesiredState(desiredStates[module.getModuleID()], isOpenLoop);
+    return ChassisSpeeds.fromFieldRelativeSpeeds(DriveConstants.KINEMATICS.toChassisSpeeds(getStates()),
+        gyroInputs.yawPosition);
   }
-  swerveSetpoint.moduleStates = optimizedStates;
-  Logger.recordOutput("states", swerveSetpoint.moduleStates);
-  return optimizedStates;
-}
 
-/**Converts robot relative {@link ChassisSpeeds} to field relative.
- * 
- * @param robotRelativeSpeeds The robot relative speeds to convert
- * @return The field relative ChassisSpeeds.
- */
-public ChassisSpeeds getFieldRelativeChassisSpeeds(ChassisSpeeds robotRelativeSpeeds) {
-  Rotation2d angle = new Rotation2d();
-  if(Robot.isSimulation()) {
-    angle = simRotation;
-  } else {
-    angle = getPose().getRotation();
+  /**
+   * Drives the robot robot-relative according to provided {@link ChassisSpeeds}.
+   * 
+   * @param chassisSpeeds The desired ChassisSpeeds. Should be robot relative.
+   */
+  public void driveRobotRelative(ChassisSpeeds chassisSpeeds) {
+    ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(chassisSpeeds, 0.02);
+    Logger.recordOutput("Unoptimized:", DriveConstants.KINEMATICS.toSwerveModuleStates(discreteSpeeds));
+    swerveSetpoint = setpointGenerator.generateSetpoint(DriveConstants.MODULE_LIMITS, swerveSetpoint, discreteSpeeds,
+        SimConstants.LOOP_TIME);
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveSetpoint.moduleStates, DriveConstants.MAX_DRIVE_SPEED);
+    setModuleStates(swerveSetpoint.moduleStates);
   }
-  return new ChassisSpeeds(
-    robotRelativeSpeeds.vxMetersPerSecond * angle.getCos()
-                  - robotRelativeSpeeds.vyMetersPerSecond * angle.getSin(),
-                  robotRelativeSpeeds.vyMetersPerSecond * angle.getCos()
-                  + robotRelativeSpeeds.vxMetersPerSecond * angle.getSin(),
-                  robotRelativeSpeeds.omegaRadiansPerSecond);
-}
 
-/**Sets the {@link IdleMode} of the DriveBase motors.
- * 
- * @param mode The braking mode (Coast or Brake) of the swerve module motors.
- */
-public void setBrakingMode(IdleMode mode) {
-  for(SwerveModule module: modules) {
-    module.setBrakingMode(mode);
+  /**
+   * Sets desired SwerveModuleStates. Optimizes states.
+   * 
+   * @param desiredStates The states to set for each module.
+   * @return The optimized SwerveModuleStates, now desired states.
+   */
+  public SwerveModuleState[] setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveModuleState[] optimizedStates = new SwerveModuleState[4];
+    // SwerveDriveKinematics.desaturateWheelSpeeds(
+    // desiredStates, DriveConstants.MAX_DRIVE_SPEED);
+    for (SwerveModule module : modules) {
+      optimizedStates[module.getModuleID()] = module.setDesiredState(desiredStates[module.getModuleID()], isOpenLoop);
+    }
+    swerveSetpoint.moduleStates = optimizedStates;
+    Logger.recordOutput("states", swerveSetpoint.moduleStates);
+    return optimizedStates;
   }
-}
 
-/**Stops all of the modules' motors. */
-public void stopMotors() {
-  for(SwerveModule module: modules) {
-    module.stop();
+  /**
+   * Converts robot relative {@link ChassisSpeeds} to field relative.
+   * 
+   * @param robotRelativeSpeeds The robot relative speeds to convert
+   * @return The field relative ChassisSpeeds.
+   */
+  public ChassisSpeeds getFieldRelativeChassisSpeeds(ChassisSpeeds robotRelativeSpeeds) {
+    Rotation2d angle = new Rotation2d();
+    if (Robot.isSimulation()) {
+      angle = simRotation;
+    } else {
+      angle = getPose().getRotation();
+    }
+    return new ChassisSpeeds(
+        robotRelativeSpeeds.vxMetersPerSecond * angle.getCos()
+            - robotRelativeSpeeds.vyMetersPerSecond * angle.getSin(),
+        robotRelativeSpeeds.vyMetersPerSecond * angle.getCos()
+            + robotRelativeSpeeds.vxMetersPerSecond * angle.getSin(),
+        robotRelativeSpeeds.omegaRadiansPerSecond);
   }
-}
 
-/**
- * @return Whether or not the controller is open loop.
- */
-public boolean isOpenLoop() {
-  return isOpenLoop;
-}
-
-/**Sets whether the controller is open loop. 
- * 
- * @param newValue The new boolean to set.
-*/
-public void setIsOpenLoop(boolean newValue) {
-  isOpenLoop = newValue;
-}
-
-/**
- * Gets gyro angle
- * @return The angle of the gyro as a {@link} Rotation2d.
- */
-public Rotation2d getGyroAngle() {
-  return gyroInputs.yawPosition;
-}
-
-@Override
-/**Runs motors during characterization voltage ramp routines.*/
-public void runVolts(double volts) {
-  for(SwerveModule module: modules) {
-    module.runVolts(volts);
+  /**
+   * Sets the {@link IdleMode} of the DriveBase motors.
+   * 
+   * @param mode The braking mode (Coast or Brake) of the swerve module motors.
+   */
+  public void setBrakingMode(IdleMode mode) {
+    for (SwerveModule module : modules) {
+      module.setBrakingMode(mode);
+    }
   }
-}
+
+  /** Stops all of the modules' motors. */
+  public void stopMotors() {
+    for (SwerveModule module : modules) {
+      module.stop();
+    }
+  }
+
+  /**
+   * @return Whether or not the controller is open loop.
+   */
+  public boolean isOpenLoop() {
+    return isOpenLoop;
+  }
+
+  /**
+   * Sets whether the controller is open loop.
+   * 
+   * @param newValue The new boolean to set.
+   */
+  public void setIsOpenLoop(boolean newValue) {
+    isOpenLoop = newValue;
+  }
+
+  /**
+   * Gets gyro angle
+   * 
+   * @return The angle of the gyro as a {@link} Rotation2d.
+   */
+  public Rotation2d getGyroAngle() {
+    return gyroInputs.yawPosition;
+  }
+
+  @Override
+  /** Runs motors during characterization voltage ramp routines. */
+  public void runVolts(double volts) {
+    for (SwerveModule module : modules) {
+      module.runVolts(volts);
+    }
+  }
 
   @Override
   public void periodic() {
-    if(Robot.isSimulation()) {
+
+    if (Robot.isSimulation()) {
       var twist = DriveConstants.KINEMATICS.toTwist2d(getPositions());
       simRotation = Rotation2d.fromDegrees(twist.dtheta);
       SmartDashboard.putNumber("Twist Theta", twist.dtheta);
@@ -248,17 +252,18 @@ public void runVolts(double volts) {
       swerveOdometry.update(gyroInputs.yawPosition, getPositions());
     }
     Optional<Pose2d> estimatedPose = photonVision.getEstimatedPose(getPose());
-    if (estimatedPose.isPresent()) swerveOdometry.addVisionMeasurement(estimatedPose.get(), photonVision.getTimestamp());
+    if (estimatedPose.isPresent())
+      swerveOdometry.addVisionMeasurement(estimatedPose.get(), photonVision.getTimestamp());
     field.setRobotPose(getPose());
     SmartDashboard.putString("Pose", getPose().toString());
     Logger.recordOutput("Odometry", getPose());
-     gyro.updateInputs(gyroInputs);
+    gyro.updateInputs(gyroInputs);
     Logger.processInputs("Drive/Gyro", gyroInputs);
     SmartDashboard.putBoolean("Field Centric", DriveConstants.FIELD_CENTRIC);
     for (var module : modules) {
       module.periodic();
     }
-  // Clear setpoint logs
+    // Clear setpoint logs
     Logger.recordOutput("SwerveStates/Desired", new double[] {});
     if (DriverStation.isDisabled()) {
       // Stop moving while disabled
@@ -268,33 +273,58 @@ public void runVolts(double volts) {
     }
 
     else {
-    Logger.recordOutput("SwerveStates/Measured", getStates());
-    Logger.recordOutput("SwerveStates/Desired", swerveSetpoint.moduleStates);
-    Logger.recordOutput("SwerveStates/SetpointSpeeds", swerveSetpoint.chassisSpeeds);
-    Logger.recordOutput("Odometry/Robot", getPose());
-    
-    //Log 3D odometry pose
-    Pose3d robotPose3d = new Pose3d(getPose());
-    robotPose3d =
-        robotPose3d
-            .exp(
-                new Twist3d(
-                    0.0,
-                    0.0,
-                    Math.abs(gyroInputs.pitchPosition.getRadians()) * RobotConstants.TRACK_WIDTH / 2.0,
-                    0.0,
-                    gyroInputs.pitchPosition.getRadians(),
-                    0.0))
-            .exp(
-                new Twist3d(
-                    0.0,
-                    0.0,
-                    Math.abs(gyroInputs.rollPosition.getRadians()) * RobotConstants.TRACK_WIDTH / 2.0,
-                    gyroInputs.rollPosition.getRadians(),
-                    0.0,
-                    0.0));
+      Logger.recordOutput("SwerveStates/Measured", getStates());
+      Logger.recordOutput("SwerveStates/Desired", swerveSetpoint.moduleStates);
+      Logger.recordOutput("SwerveStates/SetpointSpeeds", swerveSetpoint.chassisSpeeds);
+      Logger.recordOutput("Odometry/Robot", getPose());
 
-    Logger.recordOutput("Odometry/Robot3d", robotPose3d);
+      // Log 3D odometry pose
+      Pose3d robotPose3d = new Pose3d(getPose());
+      robotPose3d = robotPose3d
+          .exp(
+              new Twist3d(
+                  0.0,
+                  0.0,
+                  Math.abs(gyroInputs.pitchPosition.getRadians()) * RobotConstants.TRACK_WIDTH / 2.0,
+                  0.0,
+                  gyroInputs.pitchPosition.getRadians(),
+                  0.0))
+          .exp(
+              new Twist3d(
+                  0.0,
+                  0.0,
+                  Math.abs(gyroInputs.rollPosition.getRadians()) * RobotConstants.TRACK_WIDTH / 2.0,
+                  gyroInputs.rollPosition.getRadians(),
+                  0.0,
+                  0.0));
+
+      Pose3d frontLeftPose3d = new Pose3d(getPose().getX(),
+          getPose().getY(), Units.inchesToMeters(8),
+          new Rotation3d(Units.degreesToRadians(180), -Units.degreesToRadians(180), Units.degreesToRadians(180)));
+      frontLeftPose3d = frontLeftPose3d
+          .exp(new Twist3d(0.0, 0.0, 0.0,
+              0.0, 0.0, getPose().getRotation().getRadians()))
+       .exp(new Twist3d(Units.inchesToMeters(14.5), Units.inchesToMeters(9), 0.0, 0.0, 0.0, 0.0));
+      Pose3d BackPose3d = new Pose3d(getPose().getX(), getPose().getY(), Units.inchesToMeters(8),
+          new Rotation3d(Units.degreesToRadians(180), -Units.degreesToRadians(180), Units.degreesToRadians(0)));
+      BackPose3d = BackPose3d
+          .exp(new Twist3d(0.0, 0.0, 0.0,
+              0.0, 0.0, getPose().getRotation().getRadians()))
+          .exp(new Twist3d(Units.inchesToMeters(8),
+              0.0, 0.0, 0.0, 0.0, 0.0));
+      Pose3d frontRightPose3d = new Pose3d(getPose().getX(),
+          getPose().getY(), Units.inchesToMeters(7),
+          new Rotation3d(Units.degreesToRadians(180), -Units.degreesToRadians(180), Units.degreesToRadians(90)));
+      frontRightPose3d = frontRightPose3d
+          .exp(new Twist3d(0.0, 0.0, 0.0,
+              0.0, 0.0, getPose().getRotation().getRadians()))
+          .exp(new Twist3d(Units.inchesToMeters(12), Units.inchesToMeters(0), 0.0, 0.0, 0.0, 0.0));
+
+      Logger.recordOutput("Back", BackPose3d);
+      Logger.recordOutput("Front Right", frontRightPose3d);
+      Logger.recordOutput("Front Left", frontLeftPose3d);
+
+      Logger.recordOutput("Odometry/Robot3d", robotPose3d);
     }
   }
 }
