c92a5a60de3ba2ac6e83cb27d84d7a990592f967
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-02-22 02:23:11 -0500

    fix auto config

--- src/main/java/frc/robot/AutoFactory.java
+++ src/main/java/frc/robot/AutoFactory.java
@@ -10,12 +10,16 @@ import org.littletonrobotics.junction.Logger;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -43,6 +47,26 @@ public class AutoFactory {
         this.intake = intake;
         this.shooter = shooter;
         this.pivot = pivot;
+
+          AutoBuilder.configureHolonomic(
+        driveBase::getPose, // Robot pose supplier
+        driveBase::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+        driveBase::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        driveBase::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+            new PIDConstants(1, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(1, 0.0, 0.0), // Rotation PID constants
+            4.5, // Max module speed, in m/s
+            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+            new ReplanningConfig() // Default path replanning config. See the API for the options here
+        ),
+        () -> {
+          return DriverStation.getAlliance().get() == DriverStation.Alliance.Red;
+        },
+        driveBase);
+
+        NoteVisualizer.setRobotPoseSupplier(driveBase::getPose, pivot::getPosition);
+
     }
 
     /**
