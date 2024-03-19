2a677cdb6012aa5d5aced66029f9b4c09e146b99
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-03-19 01:08:22 -0400

    wip: Proof-of-concept LED subsystem and patterns

--- src/main/java/frc/robot/subsystems/leds/LEDs.java
+++ src/main/java/frc/robot/subsystems/leds/LEDs.java
@@ -1,15 +1,52 @@
 package frc.robot.subsystems.leds;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LEDConstants;
+import stl.led.AlphaBuffer;
+import stl.led.LobstahLEDBuffer;
 
 public class LEDs extends SubsystemBase {
     LEDIO io;
+
+    boolean possession = false;
+
+    Timer possessionSignalTimer = new Timer();
 
     public LEDs(LEDIO io) {
         this.io = io;
     }
 
+    public void setPossession(boolean value) {
+        possession = value;
+        if (possession) possessionSignalTimer.restart();
+    }
+
     public void periodic() {
-        
+        io.setData(LobstahLEDBuffer.layer(LEDConstants.LED_LENGTH,
+            posessionSignal(),
+            posessionIndicator(),
+            inMatchPattern()
+        ).toAdressableLEDBuffer());
+    }
+
+    LobstahLEDBuffer posessionSignal() {
+        double t = possessionSignalTimer.get();
+        if (t>= 1 || !possession) return null;
+
+        return LobstahLEDBuffer.solid(LEDConstants.LED_LENGTH, new Color(77, 255, 79))
+            .opacity(1-t);
+    }
+
+    LobstahLEDBuffer posessionIndicator() {
+        if (!possession) return null;
+        return LobstahLEDBuffer.solid(5, new Color(77, 255, 79))
+            .wrappedShift(LEDConstants.LED_LENGTH, -5);
+    }
+
+    LobstahLEDBuffer inMatchPattern() {
+        return LobstahLEDBuffer.solid(LEDConstants.LED_LENGTH, new Color(255, 69, 118))
+                .mask(AlphaBuffer.sine(LEDConstants.LED_LENGTH, 1.0 / 10, Timer.getFPGATimestamp() * 5));
     }
 }
