8cdfb3aede309fc5afeb155bf3a60be282f44555
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-03-18 23:02:02 -0400

    feat: Add multiply, layer, and invert methods to LobstahLEDBuffer

--- src/main/java/stl/led/AlphaBuffer.java
+++ src/main/java/stl/led/AlphaBuffer.java
@@ -18,4 +18,28 @@ public class AlphaBuffer {
         this(length);
         Arrays.fill(buffer, alpha);
     }
+
+    public static AlphaBuffer multiply(AlphaBuffer a, AlphaBuffer b) {
+        double[] result = new double[a.buffer.length];
+        for (int i = 0; i < a.buffer.length; i++) {
+            result[i] = a.buffer[i] * b.buffer[i];
+        }
+        return new AlphaBuffer(result);
+    }
+
+    public static AlphaBuffer layer(AlphaBuffer a, AlphaBuffer b) {
+        double[] result = new double[a.buffer.length];
+        for (int i = 0; i < a.buffer.length; i++) {
+            result[i] = a.buffer[i] + b.buffer[i] * (1 - a.buffer[i]);
+        }
+        return new AlphaBuffer(result);
+    }
+
+    public static AlphaBuffer invert(AlphaBuffer a) {
+        double[] result = new double[a.buffer.length];
+        for (int i = 0; i < a.buffer.length; i++) {
+            result[i] = 1 - a.buffer[i];
+        }
+        return new AlphaBuffer(result);
+    }
 }
