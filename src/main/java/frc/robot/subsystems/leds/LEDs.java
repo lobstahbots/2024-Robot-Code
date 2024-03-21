187eed6a9800997391abf0400b04c448075ba747
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-03-20 20:41:06 -0400

    wip: Design LED Patterns

--- src/main/java/frc/robot/subsystems/leds/LEDs.java
+++ src/main/java/frc/robot/subsystems/leds/LEDs.java
@@ -2,6 +2,7 @@
 
 package frc.robot.subsystems.leds;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
@@ -14,7 +15,22 @@ import stl.led.LobstahLEDBuffer;
 public class LEDs extends SubsystemBase {
     LEDIO io;
 
+    public enum FMSState { DISCONNECTED, DS_ONLY, RED, BLUE }
+    FMSState fmsState = FMSState.DISCONNECTED;
+    public enum RobotMode { DISABLED, TELEOP, AUTONOMOUS, ESTOPPED }
+    RobotMode robotMode = RobotMode.DISABLED;
+    boolean intaking = false;
     boolean possession = false;
+    public enum AutoAssistState { INACTIVE, ACTIVE, DONE }
+    AutoAssistState autoAssist = AutoAssistState.INACTIVE;
+    boolean shooterReady = false;
+    boolean ampSignal = false;
+    boolean userSignal = false;
+    boolean driveOverheated = false;
+    boolean otherSubsytemOverheated = false;
+    boolean tipped = false;
+    boolean coastMode = false;
+    boolean lowBattery = false;
 
     public LEDs(LEDIO io) {
         this.io = io;
@@ -27,13 +43,61 @@ public class LEDs extends SubsystemBase {
         if (possession) possessionSignalTimer.restart();
     }
 
+    public void setIntaking (boolean value) { intaking = value; }
+
+    public void setAutoAssist(AutoAssistState value) { autoAssist = value; }
+
+    public void setShooterReady(boolean value) { shooterReady = value; }
+
+    public void setAmpSignal(boolean value) { ampSignal = value; }
+
+    public void setUserSignal(boolean value) { userSignal = value; }
+
+    public void setDriveOverheated(boolean value) { driveOverheated = value; }
+
+    public void setOtherSubsystemOverheated(boolean value) { otherSubsytemOverheated = value; }
+
+    public void setTipped(boolean value) { tipped = value; }
+
+    public void setCoastMode(boolean value) { coastMode = value; }
+
+    public void setLowBattery(boolean value) { lowBattery = value; }
+
+    public void triggerTeleopCountdown() { }
+
+    public void triggerEndgameSignal() { }
+
     public void periodic() {
         loadingNotifier.stop();
+
+        if (!DriverStation.isDSAttached()) {
+            fmsState = FMSState.DISCONNECTED;
+        } else if (DriverStation.isFMSAttached()) {
+            if (DriverStation.getAlliance().ge == DriverStation.Alliance.Red) {
+                fmsState = FMSState.RED;
+            } else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+                fmsState = FMSState.BLUE;
+            } else {
+                fmsState = FMSState.DS_ONLY;
+            }
+        } else {
+            fmsState = FMSState.DS_ONLY;
+        }
+
+        if (DriverStation.isEStopped()) {
+            robotMode = RobotMode.ESTOPPED;
+        } else if (DriverStation.isAutonomousEnabled()) {
+            robotMode = RobotMode.AUTONOMOUS;
+        } else if (DriverStation.isTeleopEnabled()) {
+            robotMode = RobotMode.TELEOP;
+        } else {
+            robotMode = RobotMode.DISABLED;
+        }
 
         io.setData(LobstahLEDBuffer.layer(LEDConstants.LED_LENGTH,
                 posessionSignal(),
                 posessionIndicator(),
-                inMatchPattern()).toAdressableLEDBuffer());
+                disabledStandby() ).toAdressableLEDBuffer());
     }
 
     private final Notifier loadingNotifier = new Notifier(() -> {
@@ -59,7 +123,7 @@ public class LEDs extends SubsystemBase {
             .wrappedShift(LEDConstants.LED_LENGTH, -5);
     }
 
-    LobstahLEDBuffer inMatchPattern() {
+    LobstahLEDBuffer disabledStandby() {
         return LobstahLEDBuffer.solid(LEDConstants.LED_LENGTH, new Color(255, 69, 118))
                 .mask(AlphaBuffer.sine(LEDConstants.LED_LENGTH, 10, Timer.getFPGATimestamp() * 5));
     }
