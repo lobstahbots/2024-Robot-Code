bdf7837607dc624fed869c3c700a7cdeca385280
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-03-23 01:00:58 -0400

    feat: Update LED Demo

--- src/main/java/frc/robot/LEDsDemo.java
+++ src/main/java/frc/robot/LEDsDemo.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import frc.robot.subsystems.leds.LEDs;
+import frc.robot.subsystems.leds.LEDs.RobotMode;
 
 public class LEDsDemo extends Thread {
     private LEDs leds;
@@ -12,10 +13,21 @@ public class LEDsDemo extends Thread {
     @Override
     public void run() {
         try {
+            leds.setRobotMode(RobotMode.DISABLED);
+            sleep(5000);
+            leds.setUserSignal(true);
+            sleep(1000);
+            leds.setUserSignal(false);
+            sleep(5000);
+            leds.setRobotMode(RobotMode.AUTONOMOUS);
+            sleep(5000);
             leds.setPossession(true);
             sleep(5000);
             leds.setPossession(false);
             sleep(5000);
+            leds.setRobotMode(RobotMode.TELEOP);
+            sleep(2000);
+            leds.setRobotMode(RobotMode.DISABLED);
 
         } catch (Exception e) {
             e.printStackTrace();
