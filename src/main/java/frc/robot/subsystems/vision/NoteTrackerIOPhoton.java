f13b87831916f126cc2ec490567c3d527f622226
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-03-31 12:36:54 -0400

    Add PhotonVision Implementation

---
+++ src/main/java/frc/robot/subsystems/vision/NoteTrackerIOPhoton.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import frc.robot.networkalerts.Alert;
+import frc.robot.networkalerts.Alert.AlertType;
+
+/** Add your docs here. */
+public class NoteTrackerIOPhoton implements NoteTrackerIO {
+    private final PhotonCamera noteCamera;
+    private final Alert cameraDisconnectedAlert;
+    private List<PhotonTrackedTarget> notes = new ArrayList<>();
+
+    public NoteTrackerIOPhoton() {
+        noteCamera = new PhotonCamera("NoteCam");
+        cameraDisconnectedAlert = new Alert("Note camera has disconnected.", AlertType.ERROR, () -> !noteCamera.isConnected());
+    }
+
+    public List<PhotonTrackedTarget> getNotes() {
+        return notes;
+    }
+
+    public void updateInputs(NoteTrackerIOInputs inputs) {
+        var result = noteCamera.getLatestResult();
+        notes = result.targets;
+        inputs.notes = notes.toArray(new PhotonTrackedTarget[] {});
+    }
+}
