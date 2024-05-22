84c91bfaa43c002a2d11c79d6eb44bb1c976b874
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-05-22 17:11:05 -0400

    Revert LED changes in "[TEMP COMMIT] Amp tuning"
    This reverts portions of commit 6c79b2506c9f39c59d3c9608ccf5f42c91a98916.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -387,19 +387,6 @@ public final class Constants {
 
   public static class LEDConstants {
     public static final int LED_PORT = 0;
-    /** @deprecated */ public static final int LED_LENGTH = 200;
-    
-    /** LED layout:
-     * B   D
-     * |\C/|
-     * A   E
-     */
-
-
-    public static final int LOWER_LEFT_LENGTH = 23; // A
-    public static final int UPPER_LEFT_LENGTH = 16; // B
-    public static final int MID_SEGMENT_LENGTH = 21;// C
-    public static final int UPPER_RIGHT_LENGTH = 18;// D
-    public static final int LOWER_RIGHT_LENGTH = 24;// E
+    public static final int LED_LENGTH = 60;
   }
 }
