59bb075ed67ac582a2fb401394b1576acea2d7dd
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-05-26 14:51:15 -0400

    test(leds): Add logBitmap to LEDDevTools

--- src/main/java/stl/led/LEDDevTools.java
+++ src/main/java/stl/led/LEDDevTools.java
@@ -1,5 +1,13 @@
 package stl.led;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 
@@ -16,5 +24,26 @@ public class LEDDevTools {
                 return defaultColor;
             }
         }
+    }
+
+    public static void logBitmap(AddressableLEDBuffer buffer, File file) {
+        BufferedImage image = new BufferedImage(buffer.getLength(), 1, BufferedImage.TYPE_INT_RGB);
+        WritableRaster raster = image.getRaster();
+
+        for (int i = 0; i < buffer.getLength(); i++) {
+            raster.setPixel(i, 0, new int[] {buffer.getRed(i), buffer.getGreen(i), buffer.getBlue(i)});
+        }
+
+        try {
+            ImageIO.write(image, "bmp", file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int logIndex = 0;
+    public static void logBitmap(AddressableLEDBuffer buffer) {
+        logBitmap(buffer, new File(Filesystem.getOperatingDirectory(), "led_log/"+logIndex+".bmp"));
+        logIndex++;
     }
 }
